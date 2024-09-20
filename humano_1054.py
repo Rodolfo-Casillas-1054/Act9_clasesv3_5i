239c26a968cc76e7b9e573a1a514a63b046e21dd
Rodolfo Casillas <164534036+Rodolfo-Casillas-1054@users.noreply.github.com>
2024-09-20 11:52:11 -0600

    Add files via upload

---
+++ humano_1054.py
@@ -0,0 +1,64 @@
+print("Act 9 clase humano")
+print("Rodolfo Casillas 22308051281054\n")
+#Zona de clases
+class Humano1054:
+    #Zona de atributos
+    edad=0
+    nombre=" "
+    apellidop=" "
+    apellidom=" "
+    estatura=" "
+    colorcabello=" "
+
+    #Zona de funciones dentro de la clase
+    def correr1054(self,n):
+        print(f"{n} está corriendo ufff....")
+
+    def comiendo1054(self,n):
+        print(f"{n} está comiendo, que rico ñamñam")
+
+    def hablando1054(self,n):
+        print(f"{n} está hablando como Nava")
+
+    def estudiando1054(self,n):
+        print(f"{n} está estudiando, que raro, que raro ")
+
+#Zona de creacion de objetos
+Rodolfo=Humano1054()
+Najera=Humano1054()
+#Zona de usando objetos
+print("Resultados para Rodolfo")
+Rodolfo.edad=16
+print(f"Edad: {Rodolfo.edad}")
+Rodolfo.nombre="Rodolfo"
+print(f"Nombre: {Rodolfo.nombre}")
+Rodolfo.apellidop="Casillas"
+print(f"Apellido Paterno: {Rodolfo.apellidop}")
+Rodolfo.apellidom="Garcia"
+print(f"Apellido Materno: {Rodolfo.apellidom}")
+Rodolfo.estatura=1.84
+print(f"Estatura: {Rodolfo.estatura}")
+Rodolfo.colorcabello="Negro"
+print(f"Color de Cabello: {Rodolfo.colorcabello}")
+Rodolfo.correr1054("Rodolfo")
+Rodolfo.comiendo1054("Rodolfo")
+Rodolfo.hablando1054("Rodolfo")
+Rodolfo.estudiando1054("Rodolfo")
+
+print("\nResultados para Najera")
+Najera.edad=17
+print(f"Edad: {Najera.edad}")
+Najera.nombre="Cesar"
+print(f"Nombre: {Najera.nombre}")
+Najera.apellidop="Najera"
+print(f"Apellido Paterno: {Najera.apellidop}")
+Najera.apellidom="Najera"
+print(f"Apellido Materno: {Najera.apellidom}")
+Najera.estatura=1.82
+print(f"Estatura: {Najera.estatura}")
+Najera.colorcabello="Negro"
+print(f"Color de Cabello: {Najera.colorcabello}")
+Najera.correr1054("Najera")
+Najera.comiendo1054("Najera")
+Najera.hablando1054("Najera")
+Najera.estudiando1054("Najera")
